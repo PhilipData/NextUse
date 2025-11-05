ebf1c40a9e432e3de2df755f04157686d69eb042
PhilipData <philippeterssonit@gmail.com>
2025-11-05 14:39:33 +0100

    update for navbar problems

--- NextUse.Client/src/app/app.config.ts
+++ NextUse.Client/src/app/app.config.ts
@@ -1,13 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations'; // optional
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
+
+    provideAnimations(),
+  ],
 };
