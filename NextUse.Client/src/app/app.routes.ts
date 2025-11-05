e2ae572824e8e3836e95baed9c68ba90b3a9f8c1
PhilipData <philippeterssonit@gmail.com>
2025-11-05 10:39:06 +0100

    Refresh index after fixing .gitignore (*.ts removed)

--- NextUse.Client/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomePage } from './components/public_pages/home-page/home-page';
-
-
-export const routes: Routes = [
-  { path: 'home', component: HomePage },
-
-];
