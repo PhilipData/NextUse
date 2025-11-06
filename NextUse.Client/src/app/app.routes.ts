91025f3e599ddf59791ddff207207a017e4d066d
PhilipData <philippeterssonit@gmail.com>
2025-11-06 13:00:14 +0100

    forgot one last thing

--- NextUse.Client/src/app/app.routes.ts
+++ NextUse.Client/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Register } from './components/public_pages/register/register';
 import { HomePage } from './components/public_pages/home-page/home-page';
 import { authGuard, noAuthGuard } from './_utils/auth.guard';
 import { ProductList } from './components/public_pages/product-list/product-list';
+import { Login } from './components/public_pages/login/login';
 
 
 export const routes: Routes = [
