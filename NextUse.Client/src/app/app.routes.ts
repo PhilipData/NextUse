cdf7ae069df0988442ac6378c17069497f3c25bd
PhilipData <philippeterssonit@gmail.com>
2025-11-06 12:57:54 +0100

    Tried To Remove SSR

--- NextUse.Client/src/app/app.routes.ts
+++ NextUse.Client/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Register } from './components/public_pages/register/register';
 import { HomePage } from './components/public_pages/home-page/home-page';
 import { authGuard, noAuthGuard } from './_utils/auth.guard';
 import { Login } from './components/public_pages/login/login';
+import { ProductList } from './components/public_pages/product-list/product-list';
 
 
 export const routes: Routes = [
 
     { path: '', component: HomePage },
     { path: 'login', component: Login, },
+    { path: 'products', component: ProductList},
     { path: 'register', component: Register, canActivate: [noAuthGuard] },
 ];
